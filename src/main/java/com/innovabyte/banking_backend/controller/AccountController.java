275fb528d5c3e3ebed4a2dd63f2f6922e8b8503c
Nicholas Homayouni <nickh1997@outlook.com>
2024-09-17 12:45:09 -0400

    added dynamic account id endpoint and repo method

--- src/main/java/com/innovabyte/banking_backend/controller/AccountController.java
+++ src/main/java/com/innovabyte/banking_backend/controller/AccountController.java
@@ -1,15 +1,14 @@
 package com.innovabyte.banking_backend.controller;
 
 import com.innovabyte.banking_backend.entity.Account;
+import com.innovabyte.banking_backend.entity.Transaction;
 import com.innovabyte.banking_backend.repository.AccountRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost/3000")
@@ -25,5 +24,15 @@ public class AccountController {
     public ResponseEntity<List<Account>> getAllAccounts() {
         List<Account> accounts = accountRepo.findAll();
         return new ResponseEntity<>(accounts, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> getAccountById(@PathVariable Integer id) {
+        Optional<Account> account = accountRepo.findById(id);
+        if (account.isPresent()) {
+            return new ResponseEntity<>(account.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/innovabyte/banking_backend/repository/AccountRepository.java
+++ src/main/java/com/innovabyte/banking_backend/repository/AccountRepository.java
@@ -4,7 +4,10 @@ import com.innovabyte.banking_backend.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
     List<Account> findAll();
+
+    Optional<Account> findById(Integer id);
 }
